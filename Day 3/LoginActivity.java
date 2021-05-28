d21d7ef645475cb3fabf341a1443d5e1100db983
Poojan Prajapati <71919300+Poojan29@users.noreply.github.com>
2021-05-28 13:03:35 +0530

    Add files via upload

---
+++ Day 3/LoginActivity.java
@@ -0,0 +1,17 @@
+package com.example.internship;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        getSupportActionBar().setBackgroundDrawable(getDrawable(R.color.red));
+        getSupportActionBar().setTitle("Login");
+    }
+}
