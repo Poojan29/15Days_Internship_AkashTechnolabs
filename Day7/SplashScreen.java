21e1be93c829e4c27aabcf10bbf054c91bf65841
Poojan Prajapati <71919300+Poojan29@users.noreply.github.com>
2021-06-14 17:28:22 +0530

    Add files via upload

---
+++ Day7/SplashScreen.java
@@ -0,0 +1,40 @@
+package com.example.akashtechnolabinternship.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.widget.ImageView;
+
+import com.example.akashtechnolabinternship.R;
+
+public class SplashScreen extends AppCompatActivity {
+
+    ImageView imageView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        imageView = findViewById(R.id.logo);
+
+        Animation animation = new AlphaAnimation(0, 1);
+        animation.setDuration(2500);
+        imageView.setAnimation(animation);
+
+        Handler handler = new Handler();
+
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },3000);
+    }
+}
